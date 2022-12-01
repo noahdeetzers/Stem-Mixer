285abbe6cacce27ff5c26d3682685df72025c022
Noah Deetz <46035536+noahdeetzers@users.noreply.github.com>
2022-11-30 18:24:48 -0600

    Cleaned up the code and got the stems working
    Fixed the indexing issue and tried to add stereo panning.

--- newMixer/mixerv2.js
+++ newMixer/mixerv2.js
@@ -4,29 +4,31 @@ const setupSamplesButton = document.querySelector(".setupSamples");
 const playSampleButton = document.querySelector(".playSample");
 const stopSampleButton = document.querySelector(".stopSample");
 let samples;
-let trackCounter = 0;
-
 
 // const samplePaths = ["https://audio.jukehost.co.uk/8C3FUJxrSlDL0mpNEAd6FycLkH8wc0vo", "https://audio.jukehost.co.uk/NWveECn23UyNMC0Rzet4BdOeE5GXAcCv", "https://audio.jukehost.co.uk/t0jIRwI2zx31GPCOdt4M3jjfI88JDr5C", "https://audio.jukehost.co.uk/KUtQ1clmpZwyO2Iuqx7LBgP3C3vDmaus", "https://audio.jukehost.co.uk/RpnOOcn2zvnSk9fxGJoNISaI2WNS3vy9"];
 
-const samplePaths = ["https://audio.jukehost.co.uk/QvAOFlnXsqXqpNl1VT3GEhBVSrc6R6Go",
- "https://audio.jukehost.co.uk/rTFD3l0OvNpLR0hD3JHN7pDebzclHyXh",
-  "https://audio.jukehost.co.uk/1ky7S4mTHgiQD29lht7LWOe9wjjDdYkX",
-   "https://audio.jukehost.co.uk/ebrqzRFIriqvYEBhaEU5Gs7uasMLvyjP",
-    "https://audio.jukehost.co.uk/wSEW6xDd3D9GC4TU5fqiPJAja1S0Ad0g",
-     "https://audio.jukehost.co.uk/BTjOKv0v6LskbWKwKD9pE3LBvEAc3WLl",
-     "https://audio.jukehost.co.uk/LSNSaZcHqSN5APaY7xzWyHFpKFgLvkbQ",
-      "https://audio.jukehost.co.uk/L7BXqcGhGS59Qni72Ov0gIPYoVeb95Et",
-       "https://audio.jukehost.co.uk/dPvPSpjt0sPVgxYl7QNufeQcmzaT8Hoj",
-        "https://audio.jukehost.co.uk/Sd4azJpg9JJ6GAGWrPMtlAIwZDwNesCg",
-         "https://audio.jukehost.co.uk/RuI5zzqWF5oXhIJMXNUklU5wPEoVpC5R",
-          "https://audio.jukehost.co.uk/demLO1UqIEtf3OjF6b0ssjeB19RSBs43",
-           "https://audio.jukehost.co.uk/DJmYGwtAwFmeGgcFnV6TrrpEmTeDj9YS",
-            "https://audio.jukehost.co.uk/4DjmqrQLjAe12Oi5SwXY76x4yKgOZCOt",
-             "https://audio.jukehost.co.uk/joFAbUlIcnDmpMBEWk0hz2jhDkOSQUNV",
-              "https://audio.jukehost.co.uk/wyQxt010mqnqMfqgTQfH7jFp8B3ks48l",
-               "https://audio.jukehost.co.uk/0qkB0twINwgqLW7ocoq6XXF9nn7iX1P8"  ]
+const audioHost = "https://audio.jukehost.co.uk/";
 
+const samplePaths = ["QvAOFlnXsqXqpNl1VT3GEhBVSrc6R6Go",
+ "rTFD3l0OvNpLR0hD3JHN7pDebzclHyXh",
+  "1ky7S4mTHgiQD29lht7LWOe9wjjDdYkX",
+   "ebrqzRFIriqvYEBhaEU5Gs7uasMLvyjP",
+    "wSEW6xDd3D9GC4TU5fqiPJAja1S0Ad0g",
+     "BTjOKv0v6LskbWKwKD9pE3LBvEAc3WLl",
+     "LSNSaZcHqSN5APaY7xzWyHFpKFgLvkbQ",
+      "L7BXqcGhGS59Qni72Ov0gIPYoVeb95Et",
+       "dPvPSpjt0sPVgxYl7QNufeQcmzaT8Hoj",
+        "Sd4azJpg9JJ6GAGWrPMtlAIwZDwNesCg",
+         "RuI5zzqWF5oXhIJMXNUklU5wPEoVpC5R",
+          "demLO1UqIEtf3OjF6b0ssjeB19RSBs43",
+           "DJmYGwtAwFmeGgcFnV6TrrpEmTeDj9YS",
+            "4DjmqrQLjAe12Oi5SwXY76x4yKgOZCOt",
+             "joFAbUlIcnDmpMBEWk0hz2jhDkOSQUNV",
+              "wyQxt010mqnqMfqgTQfH7jFp8B3ks48l",
+               "0qkB0twINwgqLW7ocoq6XXF9nn7iX1P8"  ];
+
+
+// TODO - seperate url and hash code of audio file
 
 // TRACKS
 // 1. 2nd lead
@@ -49,19 +51,17 @@ const samplePaths = ["https://audio.jukehost.co.uk/QvAOFlnXsqXqpNl1VT3GEhBVSrc6R
 
 
 // volume
-let gainNode1;
-let volumeControl1;
-let gainNode2;
-let volumeControl2;
+// let gainNode1;
+// let volumeControl1;
+// let gainNode2;
+// let volumeControl2;
 
-// let gainNodes = [];
-// let volumeControls = [];
-
+let gainNodes = [];
+let volumeControls = [];
+let panNodes = [];
+let panControls = [];
 
 startCtxButton.addEventListener("click", () => {
-
-
-
     console.log("audio context started");
 
 
@@ -75,52 +75,46 @@ startCtxButton.addEventListener("click", () => {
             playSampleButton.setAttribute( 'aria-checked', playButtonState ? "false" : "true" );
 
 
-            // let tracks = []
-            // // REFACTOR WITH LOOP??
-            // for (const i of samples) {
-            //     tracks[i] = playSample(samples[i],0,i);
-            // }
 
-            // const track1 = playSample(samples[0], 0);
-            // const track2 = playSample(samples[1], 0);
-            // const track3 = playSample(samples[2], 0);
-            const track4 = playSample(samples[3], 0,3);
-            // const track5 = playSample(samples[4], 0);
-            // const track6 = playSample(samples[5], 0);
-            const track7 = playSample(samples[6], 0,6);
-            // const track8 = playSample(samples[7], 0);
-            // const track9 = playSample(samples[8], 0);
-            // const track10 = playSample(samples[9], 0);
-            // const track11 = playSample(samples[10], 0);
-            // const track12 = playSample(samples[11], 0);
-            // const track13 = playSample(samples[12], 0);
-            // const track14 = playSample(samples[13], 0);
-            // const track15 = playSample(samples[14], 0);
-            // const track16 = playSample(samples[15], 0);
-            // const track17 = playSample(samples[16], 0);
+                const track1 = playTracks(samples[0], 0);
+                const track2 = playTracks(samples[1], 1);
+                const track3 = playTracks(samples[2], 2);
+                const track4 = playTracks(samples[3], 3);
+                const track5 = playTracks(samples[4], 4);
+                const track6 = playTracks(samples[5], 5);
+                const track7 = playTracks(samples[6], 6);
+                const track8 = playTracks(samples[7], 7);
+                const track9 = playTracks(samples[8], 8);
+                const track10 = playTracks(samples[9], 9);
+                const track11 = playTracks(samples[10], 10);
+                const track12 = playTracks(samples[11], 11);
+                const track13 = playTracks(samples[12], 12);
+                const track14 = playTracks(samples[13], 13);
+                const track15 = playTracks(samples[14], 14);
+                const track16 = playTracks(samples[15], 15);
+                const track17 = playTracks(samples[16], 16);
 
 
-            stopSampleButton.addEventListener("click", () => {
-                track1.stop();
-                track2.stop();
-                track3.stop();
-                track4.stop();
-                track5.stop();
-
-                track6.stop();
-                track7.stop();
-                track8.stop();
-                track9.stop();
-                track10.stop();
-                track11.stop();
-                track12.stop();
-                track13.stop();
-                track14.stop();
-                track15.stop();
-                track16.stop();
-                track17.stop();
-            })    
-        });
+                stopSampleButton.addEventListener("click", () => {
+                    track1.stop();
+                    track2.stop();
+                    track3.stop();
+                    track4.stop();
+                    track5.stop();
+                    track6.stop();
+                    track7.stop();
+                    track8.stop();
+                    track9.stop();
+                    track10.stop();
+                    track11.stop();
+                    track12.stop();
+                    track13.stop();
+                    track14.stop();
+                    track15.stop();
+                    track16.stop();
+                    track17.stop();
+                })    
+        }   );
 
     });
 });
@@ -138,14 +132,19 @@ async function getFile(filePath) {
 
 
 async function setupSamples(paths) {
+    let trackCounter = 0;
     console.log("setting up samples");
     const audioBuffers = [];
     for (const path of paths) {
-        const sample = await getFile(path);
+        const sample = await getFile(audioHost + path);
         audioBuffers.push(sample);
-        trackCounter++;
         console.log("track: " + trackCounter + "/" + samplePaths.length);
-        move();
+        
+        let percentage = (trackCounter/samplePaths.length) * 100;
+       
+        addTrack(trackCounter++);
+        moveProgressBar(percentage);
+
     }
     console.log("set up complete")
     return audioBuffers;
@@ -159,20 +158,35 @@ async function setupSamples(paths) {
 //     sampleSource.start(time);
 //     return sampleSource;
 // }
-function playSample(audioBuffer, time,idx) {
-    let sampleSources = [];
-    sampleSources[idx] = audioContext.createBufferSource();
-    sampleSources[idx].buffer = audioBuffer;
-    sampleSources[idx].connect(gainNode1);
+// function playSample(audioBuffer, time,idx) {
+//     let sampleSource;
+//     sampleSource = audioContext.createBufferSource();
+//     sampleSource.buffer = audioBuffer;
 
-    gainNode1.connect(audioContext.destination);
-    sampleSources[idx].start(time);
-    return sampleSources[1];
+//     sampleSource.connect(gainNodes[idx]);
+//     gainNodes[idx].connect(audioContext.destination);
+//     sampleSource.start(time);
+//     return sampleSource;
+// }
+
+function playTracks(audioBuffer, idx) {
+
+    let sampleSource;
+    sampleSource = audioContext.createBufferSource();
+    sampleSource.buffer = audioBuffer;
+
+    sampleSource.connect(gainNodes[idx]);
+    gainNodes[idx].connect(panNodes[idx]);
+    panNodes[idx].connect(audioContext.destination);
+    // gainNodes[idx].connect(audioContext.destination);
+    sampleSource.start(0);
+
+    
 }
 
 
 
-function move() {
+function moveProgressBar(percentage) {
     var elem = document.getElementById("myBar");
     var width = 0;
     var id = setInterval(frame, 10);
@@ -181,68 +195,52 @@ function move() {
         clearInterval(id);
       } else {
         width++;
-        elem.style.width = (trackCounter/samplePaths.length) * 100 + "%";
-        elem.innerHTML = (trackCounter/samplePaths.length) * 100  + "%";
+        elem.style.width = percentage + "%";
+        elem.innerHTML = percentage  + "%";
       }
     }
-    addTrack();
   }
 
-function addTrack() {
-    let volumeId = 0;
+function addTrack(_trackCounter) {
     let div = document.createElement("div");
-    div.id = 'track' + trackCounter;
+    div.id = 'track' + _trackCounter;
     div.className = 'track';
-    div.innerHTML = `<input type="range" id=${'volume' + trackCounter} min="0" max="2" value="1" step="0.01"/>
-       <label for=${'volume' + trackCounter}>VOL</label>
+    div.innerHTML = `<input class="slider" type="range" id=${'volume' + _trackCounter} min="0" max="2" value="1" step="0.01"/>
+       <label for=${'volume' + _trackCounter}>VOL</label>
     
-       <input type="range" id=${'panner' + trackCounter} min="-1" max="1" value="0" step="0.01"/>
-       <label for=${'panner' + trackCounter}>PAN</label>
+       <input class= "slider" type="range" id=${'panner' + _trackCounter} min="-1" max="1" value="0" step="0.01"/>
+       <label for=${'panner' + _trackCounter}>PAN</label>
       
-       <button id=${'onOff' + trackCounter} class="onOffButton" aria-checked="false" data-power="on">
-           <label for=${'onOff' + trackCounter}>ON/OFF</label>
+       <button id=${'onOff' + _trackCounter} class="onOffButton" aria-checked="false" data-power="on">
+           <label for=${'onOff' + _trackCounter}>ON/OFF</label>
        </button>;`
 
     var element = document.getElementById("trackList");
     element.appendChild(div);
 
-    // gainNodes[trackCounter] = audioContext.createGain();
-    // volumeControls[trackCounter] = document.getElementById('volume' + trackCounter);
-    // volumeControls[trackCounter].addEventListener('input', function(e) {
-    //     gainNodes[trackCounter].gain.value = this.value;
-    //     console.log(e.target.id + ": " + this.value);
-    // }, false);
 
-    // volume
-    gainNode1 = audioContext.createGain();
-    volumeControl1 = document.getElementById('volume1');
-    volumeControl1.addEventListener('input', function(e) {
-        gainNode1.gain.value = this.value;
+
+    // CREATE AND MAP THE GAIN NODES TO THE TRACKS
+    gainNodes[_trackCounter] = audioContext.createGain();
+    volumeControls[_trackCounter] = document.getElementById('volume' + _trackCounter);
+    volumeControls[_trackCounter].addEventListener('input', function(e) {
+        gainNodes[_trackCounter].gain.value = this.value;
         console.log(e.target.id + ": " + this.value);
+    }, false);
+
+    console.log(typeof(gainNodes[_trackCounter]));
+    console.log("Gain Node: " + _trackCounter + " is connected to slider# " + _trackCounter);
+
+
+    // CREATE AND MAP PANNING VALUES TO TRACKS
+    panNodes[_trackCounter] = audioContext.createStereoPanner();
+    panControls[_trackCounter] = document.getElementById('panner' + _trackCounter);
+    panControls[_trackCounter].addEventListener('input', function(e) {
+        panNodes[_trackCounter].pan.value = this.value;
+        // console.log(e.target.id + ": " + this.value);
+        console.log(e.target.id + ": " + panNodes[_trackCounter].pan.value);
+
 
     }, false);
 
 }
-
-
-// // volume
-// const gainNode1 = audioContext.createGain();
-// const volumeControl1 = document.getElementById('volume1');
-// volumeControl1.addEventListener('input', function() {
-// 	gainNode1.gain.value = this.value;
-// }, false);
-
-
-//   `<div class="entry">${value} <img class="" src= "${image}"></div>`
-
-// <div id="track1" class="track"> DRUMS
-//   <input type="range" id="volume1" min="0" max="2" value="1" step="0.01"/>
-//   <label for="volume1">VOL</label>
-
-//   <input type="range" id="panner1" min="-1" max="1" value="0" step="0.01"/>
-//   <label for="panner1">PAN</label>
-  
-//   <button id="onoff1" class="onOffButton" aria-checked="false" data-power="on">
-//       <label for="onoff1">ON/OFF</label>
-//   </button>
-// </div>
